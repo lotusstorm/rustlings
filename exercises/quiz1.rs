0e27b74e1999359d58cd2374a39e8d96a0ca2770
Islam <iutizev@gmail.com>
2023-07-07 17:19:32 +0000

    day 2

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,15 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(apples_count: i32) -> i32 {  
+    if (apples_count > 40) {
+        apples_count
+    } else {
+        apples_count * 2
+    }
+}
 
 // Don't modify this function!
 #[test]
